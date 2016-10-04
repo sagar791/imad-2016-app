69501035ad845fd2b56ae05d135178689db7c59e
sagar791 <sagarshringarpure791@gmail.com>
2016-10-04 19:24:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,10 @@
 console.log('Loaded!');
 var element=document.getElementById("main-text");
+
+//move the image
+
+var img=document.getElementById('sag');
+img.onclick=function(){
+    img.style.marginLeft="100px";
+    
+};
